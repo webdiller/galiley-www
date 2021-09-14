6df6f1b764b4a9540b272ab5e6547e9d839c535f
Eugene Butkov <eugenefromrus@gmail.com>
2021-09-14 11:05:07 +1000

    промежуточный

---
+++ components/copyright.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Copyright() {
+  return (
+    <div className="copyright">
+      <div className="container copyright__wrapper">
+        <a className="copyright__logo-link">
+          <img
+            src="https://www.glofox.com/wp-content/uploads/2021/04/glofox-logo-black.svg"
+            className="copyright__logo-img"
+            alt="glofox-logo-black"
+            loading="lazy"
+            height="32"
+            width="124"
+          />
+        </a>
+
+        <div className="copyright__rights">
+          <strong>©2021 Galiley Enterprise</strong> — Все права защищены
+        </div>
+
+        <nav className="copyright__socials">
+          <a href="#" className="copyright__social"></a>
+          <a href="#" className="copyright__social"></a>
+          <a href="#" className="copyright__social"></a>
+          <a href="#" className="copyright__social"></a>
+        </nav>
+      </div>
+    </div>
+  );
+}
